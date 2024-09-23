f077b05e10d5204b5b24097c3094555aa0ba9eb3
Melys09 <melys0919@gmail.com>
2024-09-23 18:35:04 -0500

    Inicio de proyecto

---
+++ src/test/java/examen/cibertec/dsw/cibertec/evaluacion_t/ApplicationTests.java
@@ -0,0 +1,13 @@
+package examen.cibertec.dsw.cibertec.evaluacion_t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
